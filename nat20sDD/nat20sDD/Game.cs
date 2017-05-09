89ea1a1a4d75f112abe87e795f7356e13d73ac8d
nick <ncyates@users.noreply.github.com>
2017-05-09 00:21:03 -0700

    add Game

---
+++ nat20sDD/nat20sDD/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nat20sDD
+{
+    public class Game
+    {
+
+        List<Hero> heroes;
+        public Game()
+        {
+            heroes = initHeroes();
+            utilityOutput();
+        }
+
+
+        //function to create heroes team
+        public List<Hero> initHeroes()
+        {
+            List<Hero> team = new List<Hero>();
+            for(int i = 0; i < 4; i++) //fix hardcode
+            {
+                Hero h = new Hero();
+                h.setName("Strong dude " + (i+1));
+                h.setHP(10);
+                h.setStr(10);
+                h.setDex(10);
+                h.setDef(10);
+                h.setlvl(1);
+                team.Add(h);                
+            }
+            return team;
+        }
+
+        public void utilityOutput()
+        {
+            for(int i = 0; i < this.heroes.Count; i++)
+            {
+                Console.WriteLine(this.heroes[i].getName());
+            }
+        }
+
+    }
+}
