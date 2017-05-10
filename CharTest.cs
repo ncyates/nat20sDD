77ade47a9bc19bc3380674e5c88805089d5e4a25
codyhacker <jakymonkeyboy@gmail.com>
2017-05-09 22:00:48 -0700

    Add files via upload
    Unit Tests (These run on mac, not sure about windows)

---
+++ CharTest.cs
@@ -0,0 +1,59 @@
+﻿//Character Unit Tests ANDROID
+using System;
+using NUnit.Framework;
+using nat20sDD;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        [SetUp]
+        public void Setup() { }
+
+
+        [TearDown]
+        public void Tear() { }
+
+        [Test]
+        public void isAlive()
+        {
+            var something = new Character();
+            Assert.True(something.isDead());
+        }
+
+        [Test]
+        public void dead()
+        {
+            var something = new Character();
+            something.setHP(0);
+
+            if (something.getHP() == 0)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.False(true);
+            }
+        }
+
+        [Test]
+        public void addItem()
+        {
+            var something = new Character();
+            var x = new Item("Bronze Sword", 0, 1, 0, 1, 0);
+            something.pickUp(x);
+            var inv = something.getInv();
+            if (inv.Count == 1)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+
+    }
+}
---
+++ ItemTest.cs
@@ -0,0 +1,25 @@
+﻿//Character Unit Tests ANDROID
+using System;
+using NUnit.Framework;
+using nat20sDD;
+
+namespace Tests
+{
+	[TestFixture]
+	public class ItemTest
+	{
+		[SetUp]
+		public void Setup() { }
+
+
+		[TearDown]
+		public void Tear() { }
+
+		[Test]
+		public void createItemGet()
+		{
+			var testItem = new Item("Bronze Sword", 0, 1, 0, 1, 0);
+			Assert.True(testItem.strength == 1);
+		}
+	}
+}
