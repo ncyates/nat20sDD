78e2189566c97f692b9ace6c12dd624b14b11fe4
taingsean <taingsean@gmail.com>
2017-05-09 23:28:26 -0700

    Added unit tests

---
+++ UnitTests.Droid/BattleTests.cs
@@ -0,0 +1,50 @@
+﻿//Battle Unit Tests ANDROID
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests.Droid
+{
+	[TestFixture]
+	public class BattleTests
+	{
+
+		[SetUp]
+		public void Setup() { }
+
+
+		[TearDown]
+		public void Tear() { }
+
+		[Test]
+		public void start()
+		{
+			Hero c = new Hero();
+			Hero c2 = new Hero();
+			Monster m = new Monster();
+			List<Hero> charList = new List<Hero>();
+			charList.Add(c);
+			charList.Add(c2);
+			Battle b = new Battle(charList);
+			Assert.True(true);
+		}
+
+
+		[Test]
+		public void attack()
+		{
+			Hero c = new Hero();
+			Hero c2 = new Hero();
+			Monster m = new Monster();
+			List<Hero> charList = new List<Hero>();
+			charList.Add(c);
+			charList.Add(c2);
+
+			Battle b = new Battle(charList);
+			c.setStr(10);
+			m.setHP(10);
+			b.attack(c, m);
+			Assert.IsTrue(m.isDead());
+		}
+	}
+}
---
+++ UnitTests.Droid/Monster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Droid
+{
+    public class Monster : Unit
+    {
+        //additional fields
+        private int pointVal;
+        private int difficulty;
+        public Monster() : base()
+        {
+            isGood = false;
+        }
+
+        public int getPoints()
+        {
+            return pointVal;
+        }
+        public int getDifficulty()
+        {
+            return difficulty;
+        }
+
+        public void setPoint(int p)
+        {
+            pointVal = p;
+        }
+        public void setDifficulty(int d)
+        {
+            difficulty = d;
+        }
+
+    }
+}
